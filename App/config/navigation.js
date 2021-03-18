c5fae72d02e7bd1d1ad8ac1031e3f54643cc86cf
Masao Kitamura <masaok@gmail.com>
2021-03-18 00:37:06 -0700

    Customizing icons

--- App/config/navigation.js
+++ App/config/navigation.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
+import ActionsList from '../screens/ActionsList'
+import ActionDetails from '../screens/ActionDetails'
 import ContactsList from '../screens/ContactsList'
 import ContactDetails from '../screens/ContactDetails'
 
@@ -32,8 +36,49 @@ const ContactsStackScreen = () => (
   </ContactsStack.Navigator>
 )
 
+const ActionsStack = createStackNavigator()
+const ActionsStackScreen = () => (
+  <ActionsStack.Navigator>
+    <ActionsStack.Screen name="ActionsList" component={ActionsList} />
+    <ActionsStack.Screen name="ActionDetails" component={ActionDetails} />
+  </ActionsStack.Navigator>
+)
+
+const AppTabs = createBottomTabNavigator()
+const AppTabsScreen = () => (
+  <AppTabs.Navigator
+    tabBarOptions={{
+      activeTintColor: 'red',
+      activeBackgroundColor: 'blue',
+    }}
+  >
+    <AppTabs.Screen
+      name="Contacts"
+      component={ContactsStackScreen}
+      options={{
+        tabBarIcon: props => (
+          // https://reactnavigation.org/docs/tab-based-navigation/#customizing-the-appearance
+          <Ionicons name="ios-construct" size={props.size} color={props.color} />
+        ),
+      }}
+    />
+    <AppTabs.Screen
+      name="Actions"
+      component={ActionsStackScreen}
+      options={{
+        tabBarIcon: props => (
+          <Ionicons name="ios-construct" size={props.size} color={props.color} />
+        ),
+      }}
+    />
+    <AppTabs.Screen name="Contacts2" component={ContactsStackScreen} />
+    <AppTabs.Screen name="Actions2" component={ActionsStackScreen} />
+  </AppTabs.Navigator>
+)
+
 export default () => (
   <NavigationContainer>
-    <ContactsStackScreen />
+    {/* <ContactsStackScreen /> */}
+    <AppTabsScreen />
   </NavigationContainer>
 )
