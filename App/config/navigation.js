d02f6641d73cb94007f669fc86fc7285f0549f95
Masao Kitamura <masaok@gmail.com>
2021-03-17 18:32:08 -0700

    Added options and screenOptions to change header background colors

--- App/config/navigation.js
+++ App/config/navigation.js
@@ -7,9 +7,28 @@ import ContactDetails from '../screens/ContactDetails'
 
 const ContactsStack = createStackNavigator()
 const ContactsStackScreen = () => (
-  <ContactsStack.Navigator>
-    <ContactsStack.Screen name="ContactsList" component={ContactsList} />
-    <ContactsStack.Screen name="ContactDetails" component={ContactDetails} />
+  <ContactsStack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: 'red' },
+    }}
+  >
+    <ContactsStack.Screen
+      name="ContactsList"
+      component={ContactsList}
+      options={{
+        headerTitle: 'Contacts',
+      }}
+    />
+    <ContactsStack.Screen
+      name="ContactDetails"
+      component={ContactDetails}
+      options={({ route }) => {
+        return {
+          headerTitle: `${route.params.contact.name.first} ${route.params.contact.name.last}`,
+          headerStyle: { backgroundColor: 'green' },
+        }
+      }}
+    />
   </ContactsStack.Navigator>
 )
 
